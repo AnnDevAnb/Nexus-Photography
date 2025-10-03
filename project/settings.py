02d23ba5b013cc078efad79247e4b538eaf04a3e
AnnDevAnb <anndevaanbu@gmail.com>
2025-10-03 21:10:26 +0530

    errorcorrected

--- project/settings.py
+++ project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", 'fallback-secret-key')
+SECRET_KEY = 'nexus_1997'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nexus-photography.onrender.com' ,  '127.0.0.1']
 
